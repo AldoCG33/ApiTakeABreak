781176854320c949643c201dc0c6985dd5380b16
AldoCG33 <al222210655@gmail.com>
2025-03-14 10:32:29 -0600

    Subo cambios a la rama principal

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('hola mundp')
+const express = require('express'); // Importamos el módulo Express
+const app = express(); // Creamos una instancia de la aplicación Express
+const PORT = 3000; // Definimos el puerto en el que escuchará el servidor
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo desde Express!');
+});
+
+// Arrancamos el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
